ddca74396cc8cf59e03e0f4a62061eb88ba78f27
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-06-02 09:58:14 +0100

    done

--- src/Context/auth.context.jsx
+++ src/Context/auth.context.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 import authService from '../Services/auth.service';
 
 // Creates React Context with shareable State data
