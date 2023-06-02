e193b778f8dc20ac8f9f5ad597d737398832c6aa
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-06-02 11:53:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom';
-import Navbar from './Components/Navbar'
+import NavComponent from './Components/Navbar';
 import IsPrivate from './Components/IsPrivate';
 import IsAnon from './Components/IsAnon';
 import SignUpPage from './Pages/SignUp';
 import LoginPage from './Pages/Login';
+
 function App() {
  
 
   return (
     <div className="App">
-    <Navbar/>
+    <NavComponent />
     <Routes>
-
-
     <Route path="/signup" element={<IsAnon><SignUpPage/></IsAnon>}/>
     <Route path="/login" element={<IsAnon><LoginPage/></IsAnon>}/>
     </Routes>
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,37 +1,40 @@
-import {Link} from 'react-router-dom';
-import {useContext} from 'react'; 
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/auth.context';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
-  const {isLoggedIn, user, logOutUser} = 
-  useContext(AuthContext);
-
+function NavComponent() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-    <div>
-    <Link to="/">
-        <button>Home</button>
-    </Link>
-    </div>
-    {isLoggedIn? (
-      <div>
-        <Link to="/create-mood">
-          <button>Tell us how you are feeling today</button>
-        </Link>
-        <button onClick={logOutUser}>Logout</button>
-        <p>{user && user.name}</p>
-      </div>
-    ): 
-    (
-      <div>
-        <Link to="/signup"><button>Signup</button></Link>
-        <Link to="/login"><button>Login</button></Link>
-      </div>
-    )
-  } 
-    </nav>
-  )
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+
+            {isLoggedIn ? (
+              <>
+                <Nav.Link as={Link} to="/create-mood">
+                  Tell us how you are feeling today
+                </Nav.Link>
+                <button onClick={logOutUser}>Logout</button>
+                <p>{user && user.name}</p>
+              </>
+            ) : (
+              <Nav.Link href="#link">Link</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar;
+export default NavComponent;
