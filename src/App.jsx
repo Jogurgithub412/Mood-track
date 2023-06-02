02fe0db2b9b9e1ff9fdbf6546e1e530426ccf8b3
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-06-02 10:12:36 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
-
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './Components/Navbar'
+import IsPrivate from './Components/IsPrivate';
+import IsAnon from './Components/IsAnon';
+import SignUpPage from './Pages/SignUp';
+import LoginPage from './Pages/Login';
 function App() {
  
 
   return (
-    <div>
+    <div className="App">
+    <Navbar/>
+    <Routes>
 
 
+    <Route path="/signup" element={<IsAnon><SignUpPage/></IsAnon>}/>
+    <Route path="/login" element={<IsAnon><LoginPage/></IsAnon>}/>
+    </Routes>
 
     </div>
   )
